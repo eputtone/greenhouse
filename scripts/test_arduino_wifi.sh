08098c01b3c99bf37517c4942a1a66cd4154cdc1
eputtone <esa.puttonen@gmail.com>
2013-05-06 21:50:06 +0300

    More reliable stability testing (includes reading sensors)

--- scripts/test_arduino_wifi.sh
+++ scripts/test_arduino_wifi.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-COUNTER=`wget -q -O - http://192.168.0.173/greenhouse/counter`
+COUNTER=`wget -q -O - http://greenhouse.endofinternet.org/greenhouse/status | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["id"]'`
 let "COUNTER_LAST = COUNTER - 1"
 
 while [ "$COUNTER" -gt "$COUNTER_LAST" ];
 do
   COUNTER_LAST=$COUNTER
-  COUNTER=`wget -q -O - http://192.168.0.173/greenhouse/counter`
-  echo $COUNTER
+  JSON_MSG=`wget -q -O - http://greenhouse.endofinternet.org/greenhouse/status`
+  echo $JSON_MSG
+  COUNTER=`echo "$JSON_MSG" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["id"]'`
+  sleep 5
 done
